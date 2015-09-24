40b0009291fc1e53b730541a995c9a58e4aeaeb1
Léo Lam <leo@innovatetechnologi.es>
2015-09-24 23:46:09 +0200

    ratp/api: Add a cache to avoid spamming the remote API
    This commit adds a simple cache for requests to the remote API, to
    avoid sending too many requests to the RATP API. The time-to-live
    value is 15 seconds.

--- ratp/api.js
+++ ratp/api.js
@@ -5,13 +5,25 @@ let needle = require("needle");
 
 let moduleLogger = log.child({ component: "ratp/api" });
 
+const ONE_SECOND = 1000;
+const TTL = 15 * ONE_SECOND;
+let cache = {};
+
 let ratpApi = {
   query(details) {
+    const url = `${API_HOST}/APIX?keyapp=${TOKEN}` +
+      "&" + details +
+      `&withText=true&apixFormat=json`;
+    let logger = moduleLogger.child({ url });
+    if (cache[url] && new Date().getTime() < cache[url].validUntil.getTime()) {
+      logger.debug("Returning promise from cache");
+      return cache[url].promise;
+    }
+    if (cache[url] && new Date().getTime() > cache[url].validUntil.getTime()) {
+      logger.debug("Removing cache entry which expired");
+      delete cache[url];
+    }
     return new Promise((resolve, reject) => {
-      let url = `${API_HOST}/APIX?keyapp=${TOKEN}` +
-        "&" + details +
-        `&withText=true&apixFormat=json`;
-      let logger = moduleLogger.child({ url });
       needle.get(url, (error, response) => {
         if (error) {
           logger.error(error, "Failed to query the RATP API");
@@ -34,7 +46,12 @@ let ratpApi = {
           logger.error({ error: data.errorMsg }, "The RATP API returned an error");
           return reject(new Error("The RATP API returned an error: " + data.errorMsg));
         }
-        return resolve(data);
+        resolve(data);
+        cache[url] = {
+          promise: Promise.resolve(data),
+          date: new Date(),
+          validUntil: new Date(new Date().getTime() + TTL),
+        };
       });
     });
   },
