c4ea16d36748500314059b42080330234fd94a8c
Léo Lam <leo@innovatetechnologi.es>
2015-10-09 22:14:11 +0200

    Reduce code duplication by moving some methods to ratpApi
    Most notably for ratp.getIssuesForAll() and ratp.getIssues(), which now
    rely on ratpApi.queryIssues() to do most of the job and parsing.

--- ratp/api.js
+++ ratp/api.js
@@ -1,6 +1,8 @@
 const TOKEN = "FvChCBnSetVgTKk324rO";
 const API_HOST = "http://apixha.ixxi.net";
 
+let co = require("co");
+let _ = require("lodash");
 let needle = require("needle");
 
 let moduleLogger = log.child({ component: "ratp/api" });
@@ -53,6 +55,76 @@ let ratpApi = {
           validUntil: new Date(new Date().getTime() + TTL),
         };
       });
+    });
+  },
+
+  queryNextStops(stationId, lineId, directionId) {
+    return co(function* () {
+      let response = yield ratpApi.query(`cmd=getNextStopsRealtime` +
+        `&stopArea=${stationId}&line=${lineId}&direction=${directionId}`);
+      let now = new Date().getTime();
+      response.nextStopsOnLines[0].nextStops
+        .forEach((stop) => {
+          if (stop.waitingTimeRaw === "Service termine"
+            || stop.waitingTimeRaw === "Train arrete") {
+            return;
+          }
+          if (stop.waitingTime < -60) {
+            moduleLogger.debug({ stop }, "Bogus data from the RATP API: waitingTime < -60");
+            return Promise.reject(new Error("The RATP API returned bogus data: waitingTime < -60"));
+          }
+          let stopTime = new Date(stop.nextStopTime).getTime();
+          let minutesUntilStop = Math.ceil((stopTime - now) / 1000 / 60);
+          if (minutesUntilStop < -1 || minutesUntilStop > 120) {
+            moduleLogger.debug({ stop }, "Bogus data from the RATP API: impossible nextStopTime");
+            return Promise.reject(new Error("The RATP API returned bogus data: impossible nextStopTime"));
+          }
+        });
+      let stops = response.nextStopsOnLines[0].nextStops
+        .filter((stop) => stop.bStopInStation && stop.nextStopTime
+          && stop.waitingTimeRaw !== "Service termine"
+          && stop.waitingTimeRaw !== "Train arrete")
+        .map((stop) => {
+          let stopTime = new Date(stop.nextStopTime).getTime();
+          let minutesUntilStop = Math.ceil((stopTime - now) / 1000 / 60);
+          let message = minutesUntilStop + " mins";
+          if (message === "1 mins") {
+            message = "1 min";
+          }
+          if (stop.waitingTimeRaw === "Train a l'approche"
+            || stop.waitingTime === -60
+            || message === "0 mins") {
+            message = "< 1 min";
+          }
+          if (stop.waitingTimeRaw === "Train a quai") {
+            message = "À quai";
+          }
+          if (stop.waitingTimeRaw === "Train retarde") {
+            message += " (R)";
+          }
+          return {
+            destination: stop.destinationName,
+            waitingTime: stop.waitingTime,
+            nextStopTime: stop.nextStopTime,
+            rawMessage: stop.waitingTimeRaw,
+            message,
+            delayed: stop.waitingTimeRaw === "Train retarde",
+          };
+        });
+      return stops;
+    });
+  },
+
+  queryIssues(query = "networkType=all&category=all") {
+    return co(function* () {
+      let events = yield ratpApi.query(`cmd=getTrafficSituation&${query}`);
+      let issues = [];
+      events.events
+        .filter((event) => !!event.incidents)
+        .forEach((event) => {
+          issues = _.union(issues, event.incidents);
+        }) || [];
+      return issues;
     });
   },
 };
--- ratp/ratp.js
+++ ratp/ratp.js
@@ -1,8 +1,6 @@
 let co = require("co");
 let _ = require("lodash");
 
-let moduleLogger = log.child({ component: "ratp" });
-
 let ratpApi = requireFromRoot("ratp/api");
 let ratpDb = requireFromRoot("ratp/database");
 
@@ -77,88 +75,15 @@ let ratp = {
   },
 
   getNextStops(stationId, lineId, directionId) {
-    return co(function* () {
-      let response = yield ratpApi.query(`cmd=getNextStopsRealtime` +
-        `&stopArea=${stationId}&line=${lineId}&direction=${directionId}`);
-      let now = new Date().getTime();
-      response.nextStopsOnLines[0].nextStops
-        .forEach((stop) => {
-          if (stop.waitingTimeRaw === "Service termine"
-            || stop.waitingTimeRaw === "Train arrete") {
-            return;
-          }
-          if (stop.waitingTime < -60) {
-            moduleLogger.debug({ stop }, "Bogus data from the RATP API: waitingTime < -60");
-            return Promise.reject(new Error("The RATP API returned bogus data: waitingTime < -60"));
-          }
-          let stopTime = new Date(stop.nextStopTime).getTime();
-          let minutesUntilStop = Math.ceil((stopTime - now) / 1000 / 60);
-          if (minutesUntilStop < -1 || minutesUntilStop > 120) {
-            moduleLogger.debug({ stop }, "Bogus data from the RATP API: impossible nextStopTime");
-            return Promise.reject(new Error("The RATP API returned bogus data: impossible nextStopTime"));
-          }
-        });
-      let stops = response.nextStopsOnLines[0].nextStops
-        .filter((stop) => stop.bStopInStation && stop.nextStopTime
-          && stop.waitingTimeRaw !== "Service termine"
-          && stop.waitingTimeRaw !== "Train arrete")
-        .map((stop) => {
-          let stopTime = new Date(stop.nextStopTime).getTime();
-          let minutesUntilStop = Math.ceil((stopTime - now) / 1000 / 60);
-          let message = minutesUntilStop + " mins";
-          if (message === "1 mins") {
-            message = "1 min";
-          }
-          if (stop.waitingTimeRaw === "Train a l'approche"
-            || stop.waitingTime === -60
-            || message === "0 mins") {
-            message = "< 1 min";
-          }
-          if (stop.waitingTimeRaw === "Train a quai") {
-            message = "À quai";
-          }
-          if (stop.waitingTimeRaw === "Train retarde") {
-            message += " (R)";
-          }
-          return {
-            destination: stop.destinationName,
-            waitingTime: stop.waitingTime,
-            nextStopTime: stop.nextStopTime,
-            rawMessage: stop.waitingTimeRaw,
-            message,
-            delayed: stop.waitingTimeRaw === "Train retarde",
-          };
-        });
-      return stops;
-    });
+    return ratpApi.queryNextStops(stationId, lineId, directionId);
   },
 
   getIssuesForAll() {
-    return co(function* () {
-      let events = yield ratpApi.query(`cmd=getTrafficSituation` +
-        `&networkType=all&category=all`);
-      let issues = [];
-      events.events
-        .filter((event) => !!event.incidents)
-        .forEach((event) => {
-          issues = _.union(issues, event.incidents);
-        }) || [];
-      return issues;
-    });
+    return ratpApi.queryIssues();
   },
 
   getIssuesForMetro() {
-    return co(function* () {
-      let events = yield ratpApi.query(`cmd=getTrafficSituation` +
-        `&networkType=groupoflines&network=1`);
-      let issues = [];
-      events.events
-        .filter((event) => !!event.incidents)
-        .forEach((event) => {
-          issues = _.union(issues, event.incidents);
-        }) || [];
-      return issues;
-    });
+    return ratpApi.queryIssues("networkType=groupoflines&network=1");
   },
 
   getIssuesForLine(lineCode) {
