611f108dffcaddd4dc9a0955b53b767ca5b82733
Léo Lam <leo@innovatetechnologi.es>
2015-10-18 14:28:36 +0200

    Profiler: Make profiler log entries trace level

--- profiler.js
+++ profiler.js
@@ -7,7 +7,7 @@ module.exports.start = (description, data) => {
       let elapsed = process.hrtime(start)[1] / 1000000;
       let duration = parseFloat(process.hrtime(start)[0] + "." + elapsed.toFixed(0), 10);
       let message = description + " took " + duration + "s";
-      log.debug(_.extend(data, {
+      log.trace(_.extend(data, {
         component: "profiler",
         description,
         duration,
