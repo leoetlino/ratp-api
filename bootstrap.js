e8c7805fc99d3a860bcb298e83a4a4504e365c65
Léo Lam <leo@innovatetechnologi.es>
2015-12-15 18:49:37 +0100

    Update to Babel 6

--- bootstrap.js
+++ bootstrap.js
@@ -16,7 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require("babel/register");
+require("babel-polyfill");
+require("babel-register");
 if (!process.env.REPL) {
   require("./app");
 } else {
